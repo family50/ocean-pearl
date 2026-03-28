698a9b3bbdfbf02538a0c1260970a0bcf06ad4e1
yousef <yy1890518@gmail.com>
2026-03-28 19:26:09 +0200

    Fix: match import casing with filename

--- src/productss.tsx
+++ src/productss.tsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { menuData } from './Products';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import './Productss.css';
+import './productss.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
